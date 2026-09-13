53d358142a3e25040f8d1a7a944624d2e2f47c95
sosuisen <hidekazu.kubota@gmail.com>
2026-09-13 23:57:44 +0900

    feat: add drawLineOnCanvas class to draw lines on a canvas and implement tests

---
+++ src/testing/draw-line-on-canvas.spec.ts
@@ -0,0 +1,14 @@
+import { describe, it, expect } from 'vitest';
+import { DrawLineOnCanvas } from './draw-line-on-canvas';
+
+describe('drawLineOnCanvas', () => {
+  // 与えられた2点間の直線を描画できる
+  it('should draw a line between two points on a canvas', () => {
+    const drawLine = new DrawLineOnCanvas();
+    drawLine.draw({ x: 0, y: 0 }, { x: 10, y: 10 }, 0xff0000);
+    const color1: number = drawLine.getPixel(0, 0);
+    expect(color1).toBe(0xff0000);
+    const color2: number = drawLine.getPixel(10, 10);
+    expect(color2).toBe(0xff0000);
+  });
+});
---
+++ src/testing/draw-line-on-canvas.ts
@@ -0,0 +1,46 @@
+import { getCssRgb } from '../color-utils';
+
+type Point = { x: number; y: number };
+
+export class DrawLineOnCanvas {
+  private canvas: HTMLCanvasElement;
+  private ctx: CanvasRenderingContext2D;
+
+  constructor() {
+    this.canvas = document.createElement('canvas');
+    const ctx = this.canvas.getContext('2d');
+    if (!ctx) {
+      throw new Error('Failed to get 2D context from texture');
+    }
+    this.ctx = ctx;
+  }
+
+  /**
+   * Draws a line segment on the canvas.
+   * @param p1 - Starting point of the line
+   * @param p2 - Ending point of the line
+   * @param color - 24-bit integer (0x000000 to 0xFFFFFF)
+   */
+  public draw(p1: Point, p2: Point, color: number): void {
+    this.canvas.width = Math.max(p1.x, p2.x) + 1;
+    this.canvas.height = Math.max(p1.y, p2.y) + 1;
+
+    this.ctx.beginPath();
+    this.ctx.moveTo(p1.x + 0.5, p1.y + 0.5);
+    this.ctx.lineTo(p2.x + 0.5, p2.y + 0.5);
+    this.ctx.strokeStyle = getCssRgb(color);
+    this.ctx.lineWidth = 1;
+    this.ctx.stroke();
+  }
+  /**
+   * Gets the color of a pixel at the specified coordinates.
+   * @param x - The x-coordinate of the pixel
+   * @param y - The y-coordinate of the pixel
+   * @returns The color of the pixel as a 24-bit integer (0x000000 to 0xFFFFFF)
+   */
+  public getPixel(x: number, y: number): number {
+    const imageData = this.ctx.getImageData(x, y, 1, 1);
+    const [r, g, b] = imageData.data;
+    return (r << 16) | (g << 8) | b;
+  }
+}
