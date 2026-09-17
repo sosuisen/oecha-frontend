6aa03c97691ffd65622e181fd4a22e167179a858
sosuisen <hidekazu.kubota@gmail.com>
2026-09-18 06:09:00 +0900

    refactor: remove the sprite from the Layer interface

--- src/layer/layer.ts
+++ src/layer/layer.ts
@@ -1,10 +1,9 @@
-import { PointData, Sprite } from 'pixi.js';
+import { PointData } from 'pixi.js';
 import { DrawingSurface, DrawOptions } from './drawing-surface';
 import { Rect } from './rect';
 
 export interface Layer {
   readonly id: string;
-  readonly layerSprite: Sprite;
   getSurface(): DrawingSurface;
   drawLine(from: PointData, to: PointData, drawOptions: DrawOptions): void;
   clearRect(rect: Rect): void;
--- src/testing/canvas-layer.ts
+++ src/testing/canvas-layer.ts
@@ -1,17 +1,15 @@
 import { Layer } from '../layer/layer';
 import { DrawingSurface, DrawOptions } from '../layer/drawing-surface';
-import { PointData, Sprite } from 'pixi.js';
+import { PointData } from 'pixi.js';
 import { Rect } from '../layer/rect';
 
 export class CanvasLayer implements Layer {
   readonly id: string;
-  readonly layerSprite: Sprite;
   private readonly surface: DrawingSurface;
 
   constructor(id: string, surface: DrawingSurface) {
     this.id = id;
     this.surface = surface;
-    this.layerSprite = new Sprite();
   }
 
   getSurface(): DrawingSurface {
