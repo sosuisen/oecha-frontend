648aad2ee022b0d88f2fe12f100d3a87d961b21b
sosuisen <hidekazu.kubota@gmail.com>
2026-09-19 21:36:14 +0900

    fix: update default background color to white

--- src/ui/background.spec.ts
+++ src/ui/background.spec.ts
@@ -4,6 +4,6 @@ import { DEFAULT_BACKGROUND_COLOR } from './background';
 describe('Background', () => {
   // デフォルトの背景色が存在する
   it('has a default background color', () => {
-    expect(DEFAULT_BACKGROUND_COLOR).toBe(0x1099bb);
+    expect(DEFAULT_BACKGROUND_COLOR).toBe(0xffffff);
   });
 });
